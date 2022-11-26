4928ff1c10d9726adcffc1bdaa716111c4dc9137
FrontendReactJS <allgloryforrobots@gmail.com>
2022-11-26 15:59:56 +0300

    'auth'

--- index.js
+++ index.js
@@ -14,6 +14,56 @@ mongoose.connect('mongodb+srv://allgloryforrobots:02121991a@cluster0.zjvmd.mongo
 const app = express() 
 app.use(express.json())
 
+app.post('/auth/login', async (req, res) => {
+
+    try {
+
+        const user = await userModel.findOne({ email: req.body.email })
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Неверный логин или пароль',
+                // message: 'Пользователь не найден'
+                // защита от хакинга
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        console.log(req.body.password, user._doc.passwordHash)
+        if (!isValidPass) {
+            return res.status(404).json({
+                message: 'Неверный логин или пароль'
+            })
+        }
+
+        const token = jwt.sign(
+            {
+            _id: user._id,
+            }, 
+            'secret',
+            {
+                expiresIn: '30d'
+            }
+        )
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось авторизоваться',
+
+        })
+    }
+
+})
+
 app.post('/auth/reqister', registerValidation, async (req, res) => {
 
     try {
@@ -25,19 +75,36 @@ app.post('/auth/reqister', registerValidation, async (req, res) => {
 
         const password = req.body.password
         const salt = await bcrypt.genSalt(10)
-        const passwordHash = await bcrypt.hash(password, salt)
+        const hash = await bcrypt.hash(password, salt)
 
         const doc = new userModel({
             email: req.body.email,
             fullName: req.body.fullName, 
             avatarUrl: req.body.avatarUrl, 
-            passwordHash
+            passwordHash: hash
         })
 
         const user = await doc.save()
-        res.json(user)
+
+        const token = jwt.sign(
+            {
+            _id: user._id,
+            }, 
+            'secret',
+            {
+                expiresIn: '30d'
+            }
+        )
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
 
     } catch (err) {
+        console.log(err)
         res.status(500).json({
             message: 'Не удалось зарегестрироваться',
 
